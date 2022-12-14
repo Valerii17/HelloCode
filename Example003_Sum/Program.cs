4adaeb056a9daf183a650d294cfe1819c8744ae7
Valerii <vershininvv70@gmail.com>
2022-12-15 02:07:56 +0700

    added random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int namberA =3;
-int namberB =5;
-int rezult = namberA + namberB;
-Console.WriteLine(rezult);
+﻿int namberA =new Random().Next(1,10);
+Console.WriteLine(namberA);
+int namberB =new Random().Next(1,10);
+Console.WriteLine(namberB );
+Console.Write(namberA + namberB);
