c8fc40bac7d5a620665a3ada7d1a34db368a52d2
Valerii <vershininvv70@gmail.com>
2022-12-15 03:36:58 +0700

    added ++

---
+++ Example008_Magic/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(40, 10);
+//Console.WriteLine("+");
+int xa = 1; 
+int ya = 1; 
+int xb = 1; 
+int yb = 20;
+int xc = 20;
+int yc = 20;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+//Console.SetCursorPosition(xb, yb);
+//Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
